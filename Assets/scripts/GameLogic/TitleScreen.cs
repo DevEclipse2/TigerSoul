676e249792fb90db388cb9ab4a1c8cd2c0f68270
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-02-01 13:44:49 +0800

    guh i forgoot about backgrounds

--- Assets/scripts/GameLogic/TitleScreen.cs
+++ Assets/scripts/GameLogic/TitleScreen.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,12 +14,20 @@ public class TitleScreen : MonoBehaviour
     public Transform Button1;
     public GameObject StrongGlow;
     Color initialCol;
-     float timer;
+    Color initialCol2;
+    float timer;
     public string SceneToSwap;
+    public GameObject Darken;
+    bool leave = false;
+    public float Exittimer;
+    public float TimerExitMax;
+    public AudioSource audiosource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Cursor.visible = false;
         initialCol =  StrongGlow.GetComponent<SpriteRenderer>().color;
+        initialCol2 =  Darken.GetComponent<SpriteRenderer>().color;
     }
     public void OnMouseX(InputValue value)
     {
@@ -30,36 +39,55 @@ public class TitleScreen : MonoBehaviour
     }
     public void OnAny()
     {
-        SceneManager.LoadScene(SceneToSwap);
+        leave = true;
     }
     // Update is called once per frame
     void Update()
     {
-        mouseScreenPos = new Vector3(mouseScreenSpaceX, mouseScreenSpaceY, 0);
-        //Debug.Log(mouseScreenPos);
-        mousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-
-        // Create a direction vector from the GameObject's position to the mouse position
-        Vector3 direction = mousePosition - transform.position;
-
-        // Calculate the angle in degrees
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Vector3 min = Vector3.Min(Button0.position, Button1.position); // Minimum corner
-        Vector3 max = Vector3.Max(Button0.position, Button1.position); // Maximum corner
-
-        // Check if the mouse is within the defined rectangle
-        if (mousePosition.x >= min.x && mousePosition.x <= max.x &&
-            mousePosition.y >= min.y && mousePosition.y <= max.y)
+        if (leave)
         {
-            timer += Time.deltaTime;
+            if(Exittimer == 0)
+            {
+                audiosource.Play();
+                Debug.Log("play");
+            }
+            Exittimer += Time.deltaTime;
+            if(TimerExitMax < Exittimer)
+            {
+                SceneManager.LoadScene(SceneToSwap);
+            }
+            Darken.GetComponent<SpriteRenderer>().color = new Color(initialCol2.r, initialCol2.g, initialCol2.b, Mathf.Clamp01(Exittimer/TimerExitMax));
+
         }
         else
         {
-            timer-= Time.deltaTime;
+            mouseScreenPos = new Vector3(mouseScreenSpaceX, mouseScreenSpaceY, 0);
+            //Debug.Log(mouseScreenPos);
+            mousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+            // Create a direction vector from the GameObject's position to the mouse position
+            Vector3 direction = mousePosition - transform.position;
+
+            // Calculate the angle in degrees
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Vector3 min = Vector3.Min(Button0.position, Button1.position); // Minimum corner
+            Vector3 max = Vector3.Max(Button0.position, Button1.position); // Maximum corner
+
+            // Check if the mouse is within the defined rectangle
+            if (mousePosition.x >= min.x && mousePosition.x <= max.x &&
+                mousePosition.y >= min.y && mousePosition.y <= max.y)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer-= Time.deltaTime;
+            }
+            timer = Mathf.Clamp01(timer);
+            StrongGlow.GetComponent<SpriteRenderer>().color = new Color(initialCol.r , initialCol.g , initialCol.b , timer);
+            // Set the rotation of the GameObject
+            origin.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
-        timer = Mathf.Clamp01(timer);
-        StrongGlow.GetComponent<SpriteRenderer>().color = new Color(initialCol.r , initialCol.g , initialCol.b , timer);
-        // Set the rotation of the GameObject
-        origin.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        
     }
 }
