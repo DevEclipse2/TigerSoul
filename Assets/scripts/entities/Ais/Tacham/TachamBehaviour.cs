3f4d82ccf2d6c366b48949c2bce97e30a073ce93
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-02-04 23:16:59 +0800

    new enemy

---
+++ Assets/scripts/entities/Ais/Tacham/TachamBehaviour.cs
@@ -0,0 +1,61 @@
+using UnityEditor.Networking.PlayerConnection;
+using UnityEngine;
+
+public class TachamBehaviour : MonoBehaviour
+{
+    FollowPath basic;
+    Collider2D DetectionArea;
+    GameObject player;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DetectionArea = GetComponent<Collider2D>();
+        basic = GetComponent<FollowPath>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Vector3 direction = player.transform.position - transform.position;
+
+            // Perform the raycast
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction.normalized, out hit))
+            {
+                if (hit.collider.CompareTag("Playyr"))
+                {
+                    //within line of sight
+                    Chase();
+
+                }
+            }
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Disengage();
+
+        }
+    }
+    void Disengage()
+    {
+        basic.enabled = true;
+    }
+    void Chase()
+    {
+        basic.enabled = false;
+    }
+    void Engage()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
