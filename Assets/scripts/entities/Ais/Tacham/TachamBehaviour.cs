ef9926ccfb6e7b0fc00325bc9704c8663cfd5565
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-02-05 16:37:59 +0800

    Update TachamBehaviour.cs

--- Assets/scripts/entities/Ais/Tacham/TachamBehaviour.cs
+++ Assets/scripts/entities/Ais/Tacham/TachamBehaviour.cs
@@ -6,11 +6,16 @@ public class TachamBehaviour : MonoBehaviour
     FollowPath basic;
     Collider2D DetectionArea;
     GameObject player;
+    public GameObject entity;
     public float chasespeed;
-    bool chasing
+    bool chasing;
+    public transform scanbox;
+    float Scanheight;
+    bool targetground;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Scanheight = scanbox.localScale.y;
         DetectionArea = GetComponent<Collider2D>();
         basic = GetComponent<FollowPath>();
     }
@@ -24,11 +29,11 @@ public class TachamBehaviour : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(transform.position, direction.normalized, out hit))
             {
-                if (hit.collider.CompareTag("Playyr"))
+                if (hit.collider.CompareTag("Player") && Mathf.Abs(hit.transform.position.y - entity.transform.position.y) < Scanheight)
                 {
                     //within line of sight
                     Chase();
-
+                    
                 }
             }
 
@@ -49,10 +54,19 @@ public class TachamBehaviour : MonoBehaviour
     void Chase()
     {
         basic.enabled = false;
+        
+    }
+    void MoveTowardsPlayer(){
+        if(targetground)
+        {
+            float left = Mathf.Clamp(player.transform.position.x - entity.transform.position , -1 ,1 );
+            
+        }
+    
     }
     void Engage()
     {
-
+        
     }
     // Update is called once per frame
     void Update()
