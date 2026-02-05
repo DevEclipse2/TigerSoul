a81f1c4c9fbbec72b7031cd1f24978698da67201
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-02-05 22:41:37 +0800

    push

--- Assets/scripts/entities/Dialogue.cs
+++ Assets/scripts/entities/Dialogue.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class Dialogue : MonoBehaviour
 {
     Collider2D collider;
+
+    bool inrange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         collider = GetComponent<Collider2D>();
     }
-
+    void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inrange = true;
+        }
+    }
+    void OnTriggerExit2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inrange = true;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
