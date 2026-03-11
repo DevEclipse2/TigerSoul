e49fb46f4f6584cdc85c3ec0bd02e4881f8c9e9f
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-03-11 16:38:11 +0800

    stuff

--- Assets/scripts/BaseClass/DialogBase.cs
+++ Assets/scripts/BaseClass/DialogBase.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class DialogBase : MonoBehaviour
 {
     public int dialogIndex;
+    public int currentChar;
     public int dialogBranch;
     public TextMeshPro textMeshPro;
     public string buffer;
+    public float CharRate;
+    public string[] currentBranch;
+    protected bool spew;
+    protected bool exit;
+    protected bool advance;
+    protected float chartimer = 0;
+    protected float timer = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void UpdateTMP(char character)
+    public virtual void UpdateTMP(char character)
     {
         if (textMeshPro != null)
         {
@@ -17,21 +26,83 @@ public class DialogBase : MonoBehaviour
             textMeshPro.text = buffer; // Update the text
         }
     }
-    public void ScrubTMP(string val)
+    public virtual void ScrubTMP(string val)
     {
         if (textMeshPro != null)
         {
             textMeshPro.text = val;
         }
     }
-    public void ClearTMP()
+    public virtual void ClearTMP()
     {
         if (textMeshPro != null)
         {
             textMeshPro.text = " "; // Update the text
         }
     }
-    
+    public void Refresh()
+    {
+        if (spew)
+        {
+            //Debug.Log("spew");
+            timer += Time.deltaTime;
+            if (timer > chartimer && dialogIndex <= (currentBranch.Length - 1))
+            {
+                timer = 0;
+                UpdateTMP(currentBranch[dialogIndex][currentChar]);
+                currentChar++;
+            }
+            if (dialogIndex < currentBranch.Length)
+            {
+                if (currentChar == currentBranch[dialogIndex].Length)
+                {
+                    spew = false;
+                    advance = true;
+                }
+            }
+            else
+            {
+                spew = false;
+                advance = true;
+            }
+        }
+    }
+    public bool TryNextLine()
+    {
+        if (exit)
+        {
+            exit = false;
+            spew = true;
+        }
+        if (advance)
+        {
+            //this switches to the next line
+            dialogIndex++;
+            //Debug.Log(currentline);
+            spew = true;
+            currentChar = 0;
+            buffer = " ";
+            ClearTMP();
+            if (dialogIndex == currentBranch.Length)
+            {
+                dialogIndex = -1;
+                exit = true;
+                return false;
+            }
+            advance = false;
+        }
+        else
+        {
+            // this makes the entire thing show up
+            advance = true;
+            Debug.Log("advance");
+            spew = false;
+            buffer = currentBranch[dialogIndex];
+            ScrubTMP(buffer);
+        }
+        return true;
+    }
+
     public virtual void ProgressDialog()
     {
         
---
+++ Assets/scripts/entities/Dialogues/ShermanCr.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEditor.U2D.Animation;
+using UnityEngine;
+
+public class ShermanCr : DialogBase
+{
+    public string[] dialog1;
+    public string[] dialogLoop1;
+    public string[] dialogLoop2;
+    public string[] dialogLoop3;
+    public string[] dialogLoop4;
+    public string[] dialogLoop5;
+    int lastLoop = 0;
+    void Start()
+    {
+        chartimer = 1 / CharRate;
+        currentBranch = dialog1;
+    }
+    public override void ProgressDialog()
+    {
+        if(!TryNextLine())
+        {
+            int nextloop = Random.Range(1, 5);
+            while(lastLoop == nextloop)
+            {
+                nextloop = Random.Range(1, 5);
+            }
+            // this branch has concluded
+            switch(lastLoop = Random.Range(1, 5))
+            {
+                case 1:
+                    currentBranch = dialogLoop1;
+                break;
+                case 2:
+                    currentBranch = dialogLoop2;
+                break;
+                case 3:
+                    currentBranch = dialogLoop3;
+                    break;
+                case 4:
+                    currentBranch = dialogLoop4;
+                    break;
+                case 5:
+                    currentBranch = dialogLoop5;
+                    break;
+            }
+        }
+    }
+        
+    
+    void Update()
+    {
+        Refresh();
+    }
+}
