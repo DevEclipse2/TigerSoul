46f36b5627439ce424cd04bf28141f6242078170
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-06-25 23:48:52 +0800

    symmetrical animation

---
+++ Assets/scripts/player/Animator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
