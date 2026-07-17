6b7fe7c2f6e9ab6ad5e8f1c053faf723795e794a
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-07-17 11:30:20 +0800

    turret pop anim

--- Assets/scripts/entities/Ais/StopDeathAnim.cs
+++ Assets/scripts/entities/Ais/StopDeathAnim.cs
@@ -6,14 +6,27 @@ public class StopDeathAnim : MonoBehaviour
     [SerializeField]
     Animator animator;
     [SerializeField]
-    Rigidbody2D rb;
+    GameObject[] turretobject;
     public bool turretToss;
     public float forceMult = 1;
+    public PhysicsMaterial2D material;
     public void StopAnim()
     {
         animator.SetBool("Dead", false);
-        rb.angularVelocity = Random.Range(-82f, 114.2f);
-        rb.linearVelocityX = Random.Range(-1.3f, 1.3f);
-        rb.linearVelocityY = Random.Range(6.2f*forceMult, 13.3f*forceMult);
+        Destroy(animator);
+    }
+    public void TurretToss()
+    {
+        foreach (GameObject gameObject in turretobject)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+            Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+            rb.mass = 40;
+            rb.sharedMaterial = material;
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.angularVelocity = Random.Range(-82f, 114.2f);
+            rb.linearVelocityX = Random.Range(-1.3f, 1.3f);
+            rb.linearVelocityY = Random.Range(6.2f * forceMult, 13.3f * forceMult);
+        }
     }
 }
