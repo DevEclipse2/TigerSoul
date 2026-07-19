86a77b63c9605b14682afe8854347f02d433fe7d
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-07-19 20:05:13 +0800

    for the trailer

--- Assets/scripts/player/movement/upgradeLoader.cs
+++ Assets/scripts/player/movement/upgradeLoader.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public static class upgradeLoader
 {
-    public static bool Dash { get; set; } = false;
-    public static bool DoubleJump { get; set; } = false;
+    public static bool Dash { get; set; } = true;
+    public static bool DoubleJump { get; set; } = true;
     public static bool GroundPound { get; set; } = false;
     public static bool Slide { get; set; } = false;
-    public static bool wallJump { get; set; } = false;
+    public static bool wallJump { get; set; } = true;
 }
